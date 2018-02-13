b73b6d75f08ec27c246a77f9d07b7f978f3fa41d
worldzb <yang641367741@gmail.com>
2018-02-13 23:46:46 +0800

    添加 vuex 作用域测试

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
     <wd-editor></wd-editor>
+    {{download}}
   </div>
 </template>
 
 <script type="text/javascript">
+  import {mapGetters} from 'vuex';
+
+
   export default{
     name:"app",
+
+    computed:{
+      ...mapGetters(['download']),
+    },
     methods:{
     	alt:function(){
     		alert("haha");
